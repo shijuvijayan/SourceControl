78124f8e46a2d7af41bf5aef0a6f5f1a4e373f97
shiju <shiju@cybraum.net>
2019-01-14 14:08:05 +0530

    Viewdidload changed

--- SourceControl/ViewController.swift
+++ SourceControl/ViewController.swift
@@ -9,9 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    let message = "Hai"
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print(message)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
